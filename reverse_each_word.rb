e3b5308a7ae6d09dde61d665be165e567d3d1cba
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-15 00:32:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.each do|sentence|
+  reverse_each_word.each do|sentence|
     puts "#{sentence}"
   end
 end
 
-def hamburger(toppings)
-  toppings.each do |topping|
-    puts "I love #{topping} on my burgers!"
-  end
-end
